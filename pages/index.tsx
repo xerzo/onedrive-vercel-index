64a4ef5905144d8668899706fa6a5298e0944371
Lazy Meister <32761826+xerzo@users.noreply.github.com>
2022-04-09 21:49:07 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import Footer from '../components/Footer'
 import Breadcrumb from '../components/Breadcrumb'
 import SwitchLayout from '../components/SwitchLayout'
 
+import Script from 'next/script'
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
@@ -15,19 +17,34 @@ export default function Home() {
         <title>{siteConfig.title}</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
+      <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800 shit">
         <Navbar />
         <div className="mx-auto w-full max-w-5xl p-4">
           <nav className="mb-4 flex items-center justify-between pl-1">
-            <Breadcrumb />
-            <SwitchLayout />
+            
           </nav>
-         
+		  <div></div>
+
+		    <div className="td1 center">
+			<div className="th1-padding">
+			<div className="th1-blur">
+			</div>
+			<div id="anime" className="th1"></div>
+			</div>
+
+			<div className="series">
+			 <p id="qoute" className="idqoute"></p>
+			<p className="idpark">~ <span id="char"></span></p>
+			</div>
+			</div>
+		
         </div>
       </main>
-
+		<Script src="https://cdn.statically.io/gh/xerzo/iamzero.github.io/master/js/some.js" />
       <Footer />
     </div>
+	
+	
   )
 }
 
