b025702e6cec18968a91535a2b4a9528bfa486fe
Lazy Meister <32761826+xerzo@users.noreply.github.com>
2022-04-10 21:11:20 +0800

    Removing Breadcrumb

--- pages/[...path].tsx
+++ pages/[...path].tsx
@@ -22,7 +22,7 @@ export default function Folders() {
         <Navbar />
         <div className="mx-auto w-full max-w-5xl p-4">
           <nav className="mb-4 flex items-center justify-between space-x-3 pl-1">
-            <Breadcrumb query={query} />
+            <Breadcrumb />
             <SwitchLayout />
           </nav>
           <FileListing query={query} />
